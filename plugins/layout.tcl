0ecc4f7f5e7d2f9e234f7a1682fd291a6620f1bd
Lawrence Woodman <lwoodman@vlifesystems.com>
2018-07-03 15:01:18 +0100

    Use default value for content in layout::render

--- plugins/layout.tcl
+++ plugins/layout.tcl
@@ -5,12 +5,7 @@ namespace eval layout {
 
 # TODO: Work out whether should merge other values from
 # TODO: config template entry into params
-proc layout::render {layoutFilename params args} {
-  switch [llength $args] {
-    0       { set content "" }
-    1       { lassign $args content }
-    default { return -code error "wrong # args" }
-  }
+proc layout::render {layoutFilename params {content {}}} {
   set config [getvar plugins layout]
   if {![dict exists $config $layoutFilename]} {
     return -code error "unknown layout: $layoutFilename"
